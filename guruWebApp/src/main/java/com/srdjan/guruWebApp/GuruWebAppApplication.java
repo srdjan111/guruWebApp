27cbd33cd5d72295c84bc02a1e5d5a36f50eaea3
srdjan111 <42468857+srdjan111@users.noreply.github.com>
2018-10-25 12:48:20 +0200

    Add files via upload

---
+++ guruWebApp/src/main/java/com/srdjan/guruWebApp/GuruWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.srdjan.guruWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruWebAppApplication.class, args);
+	}
+}
